7c10cccba6a0af4642b9bcb61fac556126e67beb
superian <superian@gmail.com>
2022-02-18 16:57:32 +0000

    Update surbma-before-after.php
    Add two more parameters to the shortcode and change surbma (Ambrus backwards) to nai (Ian backwards) to avoid conflicts.

--- surbma-before-after.php
+++ surbma-before-after.php
@@ -10,6 +10,10 @@ Version: 2.1
 Author: CherryPick Studios
 Author URI: https://www.cherrypickstudios.com/
 
+Version 2.2
+
+Author: Ian
+
 License: GPLv2
 
 Text Domain: surbma-before-after
@@ -20,35 +24,37 @@ Domain Path: /languages/
 if ( !defined( 'ABSPATH' ) ) exit( 'Good try! :)' );
 
 // Localization
-function surbma_before_after_init() {
-	load_plugin_textdomain( 'surbma-before-after', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
+function nai_before_after_init() {
+	load_plugin_textdomain( 'nai-before-after', false, plugin_basename( dirname( __FILE__ ) ) . '/languages' );
 }
-add_action( 'plugins_loaded', 'surbma_before_after_init' );
+add_action( 'plugins_loaded', 'nai_before_after_init' );
 
-function surbma_before_after_scripts( ) {
-	wp_enqueue_script( 'surbma-before-after-event-move', plugins_url( '', __FILE__ ) . '/js/jquery.event.move.js', array('jquery'), '2.0.0', true );
-	wp_enqueue_script( 'surbma-before-after-twentytwenty', plugins_url( '', __FILE__ ) . '/js/jquery.twentytwenty.js', array('jquery'), '1.0.0', true );
+function nai_before_after_scripts( ) {
+	wp_enqueue_script( 'nai-before-after-event-move', plugins_url( '', __FILE__ ) . '/js/jquery.event.move.js', array('jquery'), '2.0.0', true );
+	wp_enqueue_script( 'nai-before-after-twentytwenty', plugins_url( '', __FILE__ ) . '/js/jquery.twentytwenty.js', array('jquery'), '1.0.0', true );
 
-	wp_enqueue_style( 'surbma-before-after-twentytwenty-style', plugins_url( '', __FILE__ ) . '/css/twentytwenty.css', array(), '1.0.0' );
+	wp_enqueue_style( 'nai-before-after-twentytwenty-style', plugins_url( '', __FILE__ ) . '/css/twentytwenty.css', array(), '1.0.0' );
 }
-add_action( 'wp_enqueue_scripts', 'surbma_before_after_scripts' );
+add_action( 'wp_enqueue_scripts', 'nai_before_after_scripts' );
 
-function surbma_before_after_footer_scripts() {
+function nai_before_after_footer_scripts() {
 ?>
 <script>jQuery(function(){jQuery(".twentytwenty-container").twentytwenty();});</script>
 <?php
 }
-add_action( 'wp_footer', 'surbma_before_after_footer_scripts', 999 );
+add_action( 'wp_footer', 'nai_before_after_footer_scripts', 999 );
 
-function surbma_before_after_shortcode( $atts ) {
+function before_after_shortcode( $atts ) {
 	extract( shortcode_atts( array(
 		'before_src' => '',
 		'before_alt' => '',
 		'after_src' => '',
 		'after_alt' => '',
 		'width' => '',
-		'height' => ''
+		'height' => '',
+		'left_description' => '',
+		'right_description' => '',
 	), $atts ) );
-	return '<div class="twentytwenty-container"><img src="'.$before_src.'" alt="'.$before_alt.'" width="'.$width.'" height="'.$height.'" /><img src="'.$after_src.'" alt="'.$after_alt.'" width="'.$width.'" height="'.$height.'" /></div>';
+	return '<div class="twentytwenty-container"><img src="'.$before_src.'" alt="'.$before_alt.'" width="'.$width.'" height="'.$height.'" /><img src="'.$after_src.'" alt="'.$after_alt.'" width="'.$width.'" height="'.$height.'" left="'.$left__description.'" right="'.$right__description.'"/></div>';
 }
-add_shortcode( 'surbma-before-after', 'surbma_before_after_shortcode' );
+add_shortcode( 'nai-before-after', 'before_after_shortcode' );
